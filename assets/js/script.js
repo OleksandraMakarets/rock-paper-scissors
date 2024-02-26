13afcd32b006be3fcfe576b791b760a72e49da9f
OleksandraMakarets <122945010+OleksandraMakarets@users.noreply.github.com>
2024-02-26 18:29:00 +0000

    Add a random selection function to randomly select rock, paper or scissors using math methods

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,15 @@ const play = (element) => {
     alert(`You picked ${element}!`);
 };
 
+//bot selects a random element
+
+const selectRandom = () => {
+    const elements = ["rock", "paper", "scissors"]; 
+    const randomNumber = Math.floor(Math.random() * 3); 
+    const randomElement = elements[randomNumber]; 
+    return randomElement; 
+};
+
 
 //add event listeners
 rock.addEventListener("click", () => play("rock"));
