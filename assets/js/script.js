e58b1219d7831b0a4bd886627e00004024b3bd24
OleksandraMakarets <122945010+OleksandraMakarets@users.noreply.github.com>
2024-02-27 23:01:15 +0000

    Create function to close the modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -146,3 +146,7 @@ const replayGame = () => {
 function openModal() {
     document.getElementById('myModal').style.display = 'flex';
 }
+// Function to close the modal
+function closeModal() {
+    document.getElementById('myModal').style.display = 'none';
+}
